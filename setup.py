3fc505e71c5a4b6fdb13ffdb3fdeb4a86bee5dca
rjw8ng <107445344+rjw8ng@users.noreply.github.com>
2022-07-11 17:18:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='HW14',
+      version='0.1',
+      description='HW14',
+      url='https://github.com/rjw8ng/M14_HW',
+      author='Royal Wang',
+      author_email='rjw8ng@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
